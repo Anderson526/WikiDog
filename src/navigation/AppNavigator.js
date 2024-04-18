fb168dd6a5e58906b026f78434118cafbd2effb1
Ander <1megaword1@gmail.com>
2024-04-18 13:00:27 -0500

    add new icons to tab menu

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import SplashScreen from "../screens/SplashScreen";
+import { Image } from "react-native";
 import DogView from "../screens/DogView";
 import CatView from "../screens/CatView";
+
 
 
 const Tab = createBottomTabNavigator();
@@ -13,13 +14,27 @@ const AppNavigator = () =>{
         return(
             <NavigationContainer>
                 <Tab.Navigator
-                      screenOptions={({ route }) => ({
-                        tabBarVisible: route.name !== 'DogView', // Oculta Screen2
-                      })}
+                    screenOptions={({route}) =>({
+                        tabBarIcon: ({focused, size})=> {
+                        let iconName;
+                        
+                        if(route.name === 'Perritos'){
+                            iconName = focused ? require('../assets/icons/IconDog.png') : require('../assets/icons/IconDog.png');
+                        }else if (route.name === 'Gaticos'){
+                            iconName = focused ? require('../assets/icons/IconCat.png') : require('../assets/icons/IconCat.png');
+                        }
+
+                        return <Image  source={iconName} style ={{width: size , height: size}}/>;
+                    },
+                })}
+                tabBarOptions ={{
+                    activeTintColor : 'purple',
+                    InactiveTintColor : 'gray',
+
+                }}
                 >
-                    <Tab.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false, tabBarStyle:{display: 'none'}, tabBarVisible: false }} />
-                    <Tab.Screen name="DogView" component={DogView} options={{ headerShown: false }} />
-                    <Tab.Screen name="CatView" component={CatView} options={{ headerShown: false }} />
+                    <Tab.Screen name="Perritos" component={DogView} options={{ headerShown: false }} />
+                    <Tab.Screen name="Gaticos" component={CatView} options={{ headerShown: false }} />
                 </Tab.Navigator>
             </NavigationContainer>
 
